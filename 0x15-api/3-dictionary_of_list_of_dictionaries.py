451133abab334c85f213b450f0d5609375fd079d
ronald0204 <ronaldaguirre0204@gmail.com>
2021-02-01 22:05:37 -0500

    All Json.2

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,37 +1,34 @@
 #!/usr/bin/python3
-
-''' Extend your Python script to export data in the Json format.
-'''
+"""Extend your Python script to export data in the Json format."""
 
 
-def export_Json_All():
+def export_to_all_json():
+    """what you did in the task #0"""
+    url_todo = 'https://jsonplaceholder.typicode.com/todos'
+    url_users = 'https://jsonplaceholder.typicode.com/users'
 
-    ''' Get all Json
-    '''
-
-    all_Url = 'http://jsonplaceholder.typicode.com/todos'
-    all_User = 'http://jsonplaceholder.typicode.com/users'
-
-    all_Data = requests.get(all_Url)
-    all_Employ = requests.get(all_User)
+    # does request in url
+    to_do = requests.get(url_todo)
+    users_todo = requests.get(url_users)
     a_dict = {}
 
-    for user in all_Employ.json():
+    for user in users_todo.json():
         username = user.get('username')
         a_list = []
-        for all_Dict in all_Data.json():
-            if all_Dict.get('userId') == user.get('id'):
+        for todo_dict in to_do.json():
+            if todo_dict.get('userId') == user.get('id'):
                 task_dict = {}
-                task_dict['task'] = all_Dict.get('title')
+                task_dict['task'] = todo_dict.get('title')
                 task_dict.update({'username': username})
-                task_dict['completed'] = all_Dict.get('completed')
+                task_dict['completed'] = todo_dict.get('completed')
                 a_list.append(task_dict)
         a_dict['{}'.format(user.get('id'))] = a_list
 
     with open("{}.json".format('todo_all_employees'), 'w', newline='') as fd:
         json.dump(a_dict, fd)
 
+
 if __name__ == '__main__':
     import json
     import requests
-    export_Json_All()
+    export_to_all_json()
